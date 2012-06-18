eca7cc81125661189e5ae738fd9ec807050f9ed5
Chris Ballinger <chrisballinger@gmail.com>
2012-06-18 11:16:32 -0700

    First commit

---
+++ Timelapse/GROAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GROAppDelegate.h
+//  Timelapse
+//
+//  Created by Chris Ballinger on 6/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GROAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
